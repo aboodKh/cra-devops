c5111c99e265aa474e386e3fc6370e2f16852402
Abdullah Khallouf <a.khallouf@outlook.com>
2020-12-06 23:52:42 +0300

    corrected a spelling mistake in app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p> testing continuos delivery to github pages using actions</p>
+        <p> testing continuous delivery to github pages using actions</p>
         <a
           className="App-link"
           href="https://reactjs.org"
