3c83697a422907620db1ac56909a5ffd11e11d30
Abdullah Khallouf <a.khallouf@outlook.com>
2020-12-07 12:18:04 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p> testing continuous delivery to github pages using actions</p>
-        <p> workflow is triggered on push or pull request to main branch  </p>
+        <p> cicd workflow is applied on normal push or merging pull request  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
